0f0bb3e54892a7d9687eb15600e62eaee77aa20a
103353377 <47434934+103353377@users.noreply.github.com>
2019-03-21 14:47:21 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,30 @@
+//Spell #1
+
+$(".Spell1").click(function(){
+ $("#spell1").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".Spell2").click(function(){
+ $("#spell2").toggle();
+});
+
+
+
+//Spell #3
+
+$(".Spell3").click(function(){
+    $("#spell3").slideToggle();
+ 
+});
+$(".disappear").click(function(){
+    $("img").delay("5").slideToggle();
+ 
+});
+
+
+
+
